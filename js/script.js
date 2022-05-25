62a4a59849d5baf655ea854db08184b76dab31df
MIST <ivanmarksaberontutoacc2@gmail.com>
2022-05-26 00:18:05 +0800

    publishing project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// fullpage scroll
+$('#fullpage').fullpage({
+    autoScrolling: true,
+    anchors: ['header', 'banner', 'bnr_info', 'bottom1', 'bottom2', 'footer'],
+    verticalCentered: false,
+    css3:false,
+});
